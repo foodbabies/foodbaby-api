ae38429a12690235126904b15133ec7e412a5031
Ryan Long <rlong@redhat.com>
2023-11-29 19:15:20 -0500

    cors

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import foodRoutes from './api/v1/routes/foodRoutes.js';
 import { logger, morganMiddleware } from './logger.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const defaultPort = 8080;
 const PORT = process.env.PORT || defaultPort;
+const corsOptions = {
+  origin: "*",
+};
 
 app.use(morganMiddleware); // todo add auth here later
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 // simple route
 app.get('/', (req, res) => {
